74c1eabb60007aaf895ba946300dd8f2277edab8
Zuziiik <zuziiiik@gmail.com>
2014-04-16 01:09:30 +0200

    WIP-style change (bootstrap)

--- View/ItemListView.php
+++ View/ItemListView.php
@@ -4,114 +4,163 @@ include_once 'View.php';
 
 class ItemListView extends View {
 
-	function __construct($model) {
-		parent::__construct($model);
-	}
+    function __construct($model) {
+        parent::__construct($model);
+    }
 
-	public function initialize() {
+    public function initialize() {
 
-	}
+    }
 
-	public function printNavigation() {
-        ?><li class="active">Recipes & Resources</li><?php
-	}
+    public function printNavigation() {
+        ?>
+        <li class="active">Recipes & Resources</li><?php
+    }
 
-	public function printBody() {
+    public function printBody() {
 
-		global $admin;
-		global $loggedIn;
-		echo($this->model->error);
-			?>
-			<form method='post' action='./index.php?page=recipes'>
-				<input type='hidden' name='action' value='filter'/>
-				<label>Filter items: <select name="items">
-						<option selected>Industrial</option>
-						<option>Vanilla</option>
-						<option>All</option>
-				</select></label>
-				<button class="submitButton" type='submit' name='filter'>Filter</button>
-			</form>
-			<?php
-			if($admin && $loggedIn) {
-				?>
-				<form name='addCategory' method='post' action='./index.php?page=recipes'>
-				<label>Add Category<input id='title' type='text' name='categoryName'/></label>
-				<button class="submitButton" type='submit'>Add</button>
-			</form>
-			<?php
-		}
+        global $admin;
+        global $loggedIn;
+        echo($this->model->error);
+        ?>
+        <h2>Recipes & Resources</h2>
+        <form method='post' action='./index.php?page=recipes'>
+            <input type='hidden' name='action' value='filter' />
+            <label>Filter items: <select class="form-control default" name="items">
+                    <option selected>Industrial</option>
+                    <option>Vanilla</option>
+                    <option>All</option>
+                </select></label>
+            <button class="btn btn-default " type='submit' name='filter'>Filter</button>
+        </form>
+        <?php
+        if ($admin && $loggedIn) {
+            ?>
+            <form name='addCategory' method='post' action='./index.php?page=recipes'>
+                <div class="form-group">
+                    <label class="col-xs-5 col-sm-3 col-md-2 col-lg-2 control-label" for="categoryName">Add
+                        Category</label>
 
-		echo("<div id='itemList'>");
-		foreach ($this->model->categories as $category) {
-			?>
-			<div class='category frame'>
-			<span class='categoryName'>
-			<?php
-			$nameCategory = $category[0]->getName();
-			$id = $category[0]->getIdCategory();
-			?>
-				<h2><?php echo($nameCategory); ?></h2>
-				<?php
-				if($loggedIn && $admin) {
+                    <div class="col-xs-5 col-sm-3 col-md-2 col-lg-2">
+                        <input type="text" class="form-control" name='categoryName' id="categoryName"></div>
+                </div>
+                <button class="btn btn-default" type='submit'>Add</button>
+            </form>
+        <?php
+        }
 
-					?>
-					<div class="categoryButtons buttonsGroup">
-						<form name='deleteCategory' method='post' action='./index.php?page=recipes'>
-							<input type='hidden' name='categoryDelete' value='<?php echo($id); ?>'/>
-							<button class="submitButton" type='submit' name='deleteCategory'>Delete Category</button>
-						</form>
-						<form class="addItem" name='addItem' method='post' action='./index.php?page=edit'>
-							<input type='hidden' name='action' value='addItem'/>
-							<input type='hidden' name='categoryName' value='<?php echo($nameCategory); ?>'/>
-							<button class="submitButton" type='submit' name='AddItem'>Add Item</button>
-						</form>
-					</div>
-				<?php
-				}
-				echo("</span>");
-				foreach ($category[1] as $item) {
-					?>
-					<div class="itemContainer frame">
-						<div class='item'>
-							<?php
-							$id = $item->getIdItem();
-							$name = $item->getName();
-							$details = $item->getDetails();
-							?>
-							<img class='itemIcon' src='image.php?type=item&id=<?php echo($id); ?>' alt="<?php echo($name); ?>'s icon">
-							<h3><a class='itemName'
-								   href='./index.php?page=item&item=<?php echo($id); ?>'><?php echo($name); ?></a></h3>
-							<div class='itemDetails'><?php echo($details); ?></div>
-							<?php
-							if($loggedIn && $admin) {
-								?>
-								<form name='deleteItem' method='post' action='./index.php?page=recipes'>
-									<input type='hidden' name='action' value='deleteItem'/>
-									<input type='hidden' name='itemId' value='<?php echo($id); ?>'/>
-									<button class="submitButton" type='submit' name='DeleteItem'>Delete</button>
-								</form>
-							<?php
-							}
-							?>
-						</div>
-					</div>
-				<?php
-				}
-				?>
-			</div>
-		<?php
+        echo("<div id='itemList'>");
+        foreach ($this->model->categories as $category) {
+            ?>
+            <div class="col-8 col-sm-8 col-md-8 col-lg-8">
+                <table class="table ">
 
-		}
+                    <?php
+                    $nameCategory = $category[0]->getName();
+                    $id = $category[0]->getIdCategory();
+                    ?>
+                    <caption class="col-5 col-sm-5 col-md-5 col-lg-5 active"><h3><?php echo($nameCategory); ?></h3>
+                    </caption>
+                    <tr>
+                        <th class="field-label col-5 col-sm-5 col-md-5 col-lg-5 active">
+                            <label>Icon</label>
+                        </th>
+                        <th class="field-label col-5 col-sm-5 col-md-5 col-lg-5 active">
+                            <label>Name</label>
+                        </th>
+                        <th class="field-label col-5 col-sm-5 col-md-5 col-lg-5 active">
+                            <label>Details</label>
+                        </th>
+                        <th class="col-5 col-sm-5 col-md-5 col-lg-5">
+                            <?php
+                            if ($loggedIn && $admin) {
+                                ?>
+                                <form>
+                                    <form name='deleteCategory' method='post' action='./index.php?page=recipes'>
+                                        <input type='hidden' name='categoryDelete' value='<?php echo($id); ?>' />
+                                        <button class="btn btn-default btn-sm " type='submit' name='deleteCategory'>
+                                            Delete
+                                            Category
+                                        </button>
+                                    </form>
+                                </form>
+                            <?php
+                            }
+                            ?>
+                        </th>
+                        <th class="col-5 col-sm-5 col-md-5 col-lg-5">
+                            <?php
+                            if ($loggedIn && $admin) {
+                                ?>
+                                <form class="addItem" name='addItem' method='post' action='./index.php?page=edit'>
+                                    <input type='hidden' name='action' value='addItem' />
+                                    <input type='hidden' name='categoryName' value='<?php echo($nameCategory); ?>' />
+                                    <button class="btn btn-default btn-sm" type='submit' name='AddItem'>Add Item
+                                    </button>
+                                </form>
 
-		echo("</div>");
-	}
+                            <?php
+                            }
+                            ?>
+                        </th>
+                    </tr>
+                    <?php
 
-	public function printPageHeader() {
-		echo("Recipes & Resources");
-	}
+                    foreach ($category[1] as $item) {
+                        $id = $item->getIdItem();
+                        $name = $item->getName();
+                        $details = $item->getDetails();
+                        ?>
+                        <tr>
+                            <td class="col-5 col-sm-5 col-md-5 col-lg-5 ">
+                                <img src="image.php?type=item&id=<?php echo($id); ?>" alt="<?php echo($name); ?>'s icon"
+                                     class="img-thumbnail" style="width: 3em; height: 3em;">
+                            </td>
+                            <td class="col-5 col-sm-5 col-md-5 col-lg-5 ">
+                                <h4><a class='btn btn-sm btn-default itemButton'
+                                       href='./index.php?page=item&item=<?php echo($id); ?>'>
+                                        <?php echo($name); ?></a></h4>
 
-	public function printTitle() {
-		echo("Industrial Craft Experimental - Wiki - Recipes & Resources");
-	}
+                            </td>
+                            <td class="col-5 col-sm-5 col-md-5 col-lg-5 ">
+                                <?php echo($details); ?>
+                            </td>
+                            <td class="col-5 col-sm-5 col-md-5 col-lg-5">
+                                <?php
+                                if ($loggedIn && $admin) {
+                                    ?>
+                                    <form name='deleteItem' method='post' action='./index.php?page=recipes'>
+                                        <input type='hidden' name='action' value='deleteItem' />
+                                        <input type='hidden' name='itemId' value='<?php echo($id); ?>' />
+                                        <button class="btn btn-sm btn-default" type='submit' name='DeleteItem'>Delete
+                                        </button>
+                                    </form>
+                                <?php
+                                }
+                                ?>
+
+                            </td>
+                            <td class="col-5 col-sm-5 col-md-5 col-lg-5">
+
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </table>
+            </div>
+        <?php
+
+        }
+
+        echo("</div>");
+    }
+
+    public function printPageHeader() {
+    }
+
+    public function printTitle() {
+        echo("Industrial Craft Experimental - Wiki - Recipes & Resources");
+    }
 
 }
